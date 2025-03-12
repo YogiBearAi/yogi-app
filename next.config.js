ea198b21f637180f572ccd76bd7a3c8c9f6f1187
YogibearAI <Simmy@sdrealtyholding.com>
2025-03-12 12:10:44 -0700

    Fixed infinite redirect loop

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/questionnaire',
-        destination: '/questionnaire/',
-        permanent: true,
-      },
-    ]
-  }
 };
 
 module.exports = nextConfig;
